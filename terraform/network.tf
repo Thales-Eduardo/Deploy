823dff42f1243eaf0bf4dfc80cd6fcd97d58f04d
Thales Eduardo <thalesdev22@gmail.com>
2024-02-08 10:22:15 -0300

    master - alterando terraform network ingress e egress.

--- terraform/network.tf
+++ terraform/network.tf
@@ -56,9 +56,32 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = ["0.0.0.0/0"] # apenas para exemplo
   }
 
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 80 # HTTP
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80 # HTTPS
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

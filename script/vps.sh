1a28c0ba0d87b7ac90f2aad7c1668bb010463a34
Thales Eduardo <thalesdev22@gmail.com>
2024-02-20 12:28:08 -0300

    master - Ajustando comandos na action configure_instance

--- script/vps.sh
+++ script/vps.sh
@@ -1,9 +1,9 @@
 git clone https://github.com/Thales-Eduardo/nginx.git app && cd app
 
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common \
+sudo apt install apt-transport-https ca-certificates curl software-properties-common \
  && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg \
  && echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
- && sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io \
+ && sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io \
  && sudo usermod -aG docker $USER
  && docker --version
 
